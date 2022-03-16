d2727f65eea5105f8112515011b021bd4c6c5219
yukistavailable <yukitatsu_0817@yahoo.co.jp>
2022-03-16 21:12:14 +0900

    second commit

--- contracts/Rounds.sol
+++ contracts/Rounds.sol
@@ -3,31 +3,64 @@
 /// @title The Rounds ERC-721 token
 
 /*
-██████   ██████  ██    ██ ███    ██ ██████
-██   ██ ██    ██ ██    ██ ████   ██ ██   ██
-██████  ██    ██ ██    ██ ██ ██  ██ ██   ██
-██   ██ ██    ██ ██    ██ ██  ██ ██ ██   ██
-██   ██  ██████   ██████  ██   ████ ██████
+██████   ██████  ██    ██ ███    ██ ██████   ██████
+██   ██ ██    ██ ██    ██ ████   ██ ██   ██ ██
+██████  ██    ██ ██    ██ ██ ██  ██ ██   ██ ███████
+██   ██ ██    ██ ██    ██ ██  ██ ██ ██   ██      ██
+██   ██  ██████   ██████  ██   ████ ██████  ███████
 */
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
 
-import "hardhat/console.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721BurnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
 
-contract Greeter {
-    string private greeting;
+contract Rounds is Initializable, ERC721Upgradeable, ERC721URIStorageUpgradeable, ERC721BurnableUpgradeable, OwnableUpgradeable, UUPSUpgradeable {
+    using CountersUpgradeable for CountersUpgradeable.Counter;
 
-    constructor(string memory _greeting) {
-        console.log("Deploying a Greeter with greeting:", _greeting);
-        greeting = _greeting;
+    CountersUpgradeable.Counter private _tokenIdCounter;
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() initializer {}
+
+    function initialize() initializer public {
+        __ERC721_init("Rounds", "ROS");
+        __ERC721URIStorage_init();
+        __ERC721Burnable_init();
+        __Ownable_init();
+        __UUPSUpgradeable_init();
     }
 
-    function greet() public view returns (string memory) {
-        return greeting;
+    function safeMint(address to, string memory uri) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
     }
 
-    function setGreeting(string memory _greeting) public {
-        console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
-        greeting = _greeting;
+    /**
+     * @dev Function reverts when `msg.sender` is not the owner.
+     * Called by {upgradeTo} and {upgradeToAndCall}.
+     */
+    function _authorizeUpgrade(address newImplementation) internal onlyOwner override {}
+
+    /*
+    * @notice Burn a token.
+    * @dev Override _burn in order to execute _burn of ERC721URIStorageUpgradeable before one of ERC721Upgradeable.
+    */
+    function _burn(uint256 tokenId) internal override(ERC721Upgradeable, ERC721URIStorageUpgradeable) {
+        super._burn(tokenId);
+    }
+
+    /*
+    * @notice URI for a token.
+    */
+    function tokenURI(uint256 tokenId) public view override(ERC721Upgradeable, ERC721URIStorageUpgradeable) returns (string memory) {
+        return super.tokenURI(tokenId);
     }
 }
